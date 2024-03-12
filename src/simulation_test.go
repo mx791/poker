22de4979c522b313fd94570eaf5b2b4cbbef9b40
Charles Lrz <56536939+mx791@users.noreply.github.com>
2024-03-12 09:24:38 +0100

    Update simulation_test.go

--- src/simulation_test.go
+++ src/simulation_test.go
@@ -16,13 +16,14 @@ func relativeErr(value float64, target float64) float64 {
 func TestPairValue(t *testing.T) {
 	myHand := []Card{MakeCard("3 of Spades"), MakeCard("3 of Hearth")}
 	value := EvalGameState(myHand, []Card{})
-	fmt.Printf("3 of Spades - 3 of Hearth p=%d", value)
+	fmt.Printf("3 of Spades - 3 of Hearth p=%f", value)
 	if relativeErr(value, 0.6) > 0.1 {
 		t.Fatalf(`Recorded %f`, value)
 	}
 
 	myHand = []Card{MakeCard("As of Spades"), MakeCard("As of Hearth")}
 	value = EvalGameState(myHand, []Card{})
+	fmt.Printf("As of Spades - As of Hearth p=%f", value)
 
 	if relativeErr(value, 0.9) > 0.1 {
 		t.Fatalf(`Recorded %f`, value)
