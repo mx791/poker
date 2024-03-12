7d08e1f2e400e67bdc29fed7ec87bc826c5f63d5
Charles Lrz <56536939+mx791@users.noreply.github.com>
2024-03-12 14:47:21 +0100

    Update game_test.go

--- src/game_test.go
+++ src/game_test.go
@@ -3,7 +3,44 @@ package main
 import (
 	"testing"
 	"fmt"
+	"math/rand"
 )
+
+
+type TalkativeRandomPlayer struct {}
+
+func (p TalkativeRandomPlayer) PlayFirst(myCards []Card, communCards []Card, totalPotValue float64) int {
+  fmt.Println("Je commence le tour, il y a %i cartes au milieu", len(communCards))
+  val := rand.Intn(5)
+  if val == 0 || val == 1 || val == 2 {
+	fmt.Println("Je check")
+    	return ACTION_CHECK
+  } else if val == 3 {
+	fmt.Println("Je relance")
+    	return ACTION_RAISE
+  }
+  fmt.Println("Je me couche")
+  return ACTION_SLEEP
+}
+
+func (p TalkativeRandomPlayer) PlayNormal(myCards []Card, communCards []Card, totalPotValue float64, betValue float64) int {
+  val := rand.Intn(5)
+  fmt.Println("A mon tour, il y a %i cartes au milieu, la mise est à %f", len(communCards), betValue)
+  if val == 0 || val == 1 || val == 2 {
+	  fmt.Println("Je suis")
+    return ACTION_FOLLOW
+  } else if val == 3 {
+	  fmt.Println("Je relance")
+    return ACTION_RAISE
+  }
+	fmt.Println("Je me couche")
+  return ACTION_SLEEP
+}
+
+func (p TalkativeRandomPlayer) ShouldFollow(myCards []Card, communCards []Card, totalPotValue float64, engagedValue float64, targetValue float64) bool {
+	fmt.Println("On me relance")
+	return rand.Intn(5) > 2
+}
 
 func comparePlayers(pA GameBot, pB GameBot) float64 {
 	aReward := 0.0
@@ -12,6 +49,13 @@ func comparePlayers(pA GameBot, pB GameBot) float64 {
 		aReward += -PlayGame(pB, pA)
 	}
 	return aReward / 200.0
+}
+
+func TestGame(t *testing.T) {
+	pA := TalkativeRandomPlayer{}
+	pB := RandomPlayer{}
+	out := PlayGame(pA, pB)
+	fmt.Println("Issue: %f", out)
 }
 
 func TestRandomGame(t *testing.T) {
