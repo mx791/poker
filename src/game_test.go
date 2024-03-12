1d1e7f327371f7d1e8756364a21328ad2de9a821
Charles Lrz <56536939+mx791@users.noreply.github.com>
2024-03-12 11:57:54 +0100

    Update game_test.go

--- src/game_test.go
+++ src/game_test.go
@@ -10,7 +10,11 @@ func TestRandomGame(t *testing.T) {
 	pB := RandomPlayer{}
 	aReward := 0.0
 	for i:=0; i<150; i++ {
-		aReward += PlayGame(pA, pB)
+		if i%2==0 {
+			aReward += PlayGame(pA, pB)
+		} else {
+			aReward += -PlayGame(pB, pA)
+		}
 	}
 	fmt.Printf("Random vs Random %f", aReward)
 }
