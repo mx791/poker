d1a669166d29b063e0b8c440b7981c61aea2801e
Charles Lrz <56536939+mx791@users.noreply.github.com>
2024-03-12 17:36:34 +0100

    Update comparator_test.go

--- src/comparator_test.go
+++ src/comparator_test.go
@@ -48,3 +48,26 @@ func TestHighestCard(t *testing.T) {
 		t.Fatalf("Error with highest card & pair")
 	}
 }
+
+func TestSuite(t *testing.T) {
+	suite := []Card{MakeCard("Queen of Spades"), MakeCard("King of Hearth"), MakeCard("10 of Hearth"), MakeCard("9 of Hearth"), MakeCard("Jack of Hearth")}
+	brelan := []Card{MakeCard("5 of Spades"), MakeCard("8 of Hearth"), MakeCard("8 of Diamonds"), MakeCard("8 of Diamonds"), MakeCard("6 of Spades")}
+
+	if CompareHands(suite, brelan) != 1 {
+		t.Fatalf("Error with suite vs brelan")
+	}
+
+	doublepaire := []Card{MakeCard("5 of Spades"), MakeCard("8 of Hearth"), MakeCard("8 of Diamonds"), MakeCard("5 of Diamonds"), MakeCard("6 of Spades")}
+	if CompareHands(suite, doublepaire) != 1 {
+		t.Fatalf("Error with suite vs double paire")
+	}
+
+	suite2 := []Card{MakeCard("5 of Spades"), MakeCard("6 of Hearth"), MakeCard("7 of Diamonds"), MakeCard("8 of Diamonds"), MakeCard("9 of Spades")}
+	if CompareHands(suite, suite2) != 1 {
+		t.Fatalf("Error with suite vs suite")
+	}
+
+	if CompareHands(suite2, brelan) != 1 {
+		t.Fatalf("Error with suite vs brelan")
+	}
+}
