0904b33e92e9450b2e3317de0ac48beba8a27c52
Charles Lrz <56536939+mx791@users.noreply.github.com>
2024-03-12 14:27:02 +0100

    Update game_test.go

--- src/game_test.go
+++ src/game_test.go
@@ -5,21 +5,20 @@ import (
 	"fmt"
 )
 
-func comparePlayers(pA GameBot, pB GameBot) (float64, float64) {
+func comparePlayers(pA GameBot, pB GameBot) float64 {
 	aReward := 0.0
-	bReward := 0.0
 	for i:=0; i<100; i++ {
-		aReward += 1.0
-		bReward += 1.0
+		aReward += PlayGame(pA, pB)
+		aReward += -PlayGame(pB, pA)
 	}
-	return aReward / 200.0, bReward/200.0
+	return aReward / 100.0
 }
 
 func TestRandomGame(t *testing.T) {
 	pA := RandomPlayer{}
 	pB := RandomPlayer{}
-	resA, resB := comparePlayers(pA, pB)
-	fmt.Printf("Random vs Random %f - %f", resA, resB)
+	resA := comparePlayers(pA, pB)
+	fmt.Printf("Random vs Random %f", resA)
 }
 
 func TestRandomvsproba(t *testing.T) {
@@ -34,6 +33,6 @@ func TestProba(t *testing.T) {
 	pA := ProbabilistPlayer{}
 	pB := ProbabilistPlayer{}
 	NUM_SIMS  = 10_000
-	resA, resB := comparePlayers(pA, pB)
-	fmt.Printf("Random vs Random %f - %f", resA, resB)
+	resA := comparePlayers(pA, pB)
+	fmt.Printf("Random vs Random %f", resA)
 }
