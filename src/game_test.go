7d85dae05ad6576504a1652e1c1fd4e6783b6df6
Charles Lrz <56536939+mx791@users.noreply.github.com>
2024-03-12 12:06:53 +0100

    Update game_test.go

--- src/game_test.go
+++ src/game_test.go
@@ -18,3 +18,18 @@ func TestRandomGame(t *testing.T) {
 	}
 	fmt.Printf("Random vs Random %f", aReward)
 }
+
+func TestRandomvsproba(t *testing.T) {
+	pA := ProbabilistPlayer{}
+	pB := RandomPlayer{}
+	aReward := 0.0
+	NUM_SIMS  = 10_000
+	for i:=0; i<150; i++ {
+		if i%2==0 {
+			aReward += PlayGame(pA, pB)
+		} else {
+			aReward += -PlayGame(pB, pA)
+		}
+	}
+	fmt.Printf("probabilist vs Random %f", aReward)
+}
