75b5bff6cdb6c27ddfd341ff095fda31124ffc98
Charles Lrz <56536939+mx791@users.noreply.github.com>
2024-03-12 12:22:23 +0100

    Update game_test.go

--- src/game_test.go
+++ src/game_test.go
@@ -5,47 +5,43 @@ import (
 	"fmt"
 )
 
-func TestRandomGame(t *testing.T) {
-	pA := RandomPlayer{}
-	pB := RandomPlayer{}
+func comparePlayers(pA GameBot, pB GameBot) (float64, float64) {
 	aReward := 0.0
+	bReward := 0.0
 	for i:=0; i<100; i++ {
 		if PlayGame(pA, pB) > 0.0 {
 			aReward += 1.0
+		} else {
+			bReward += 1.0
 		}
 		if PlayGame(pB, pA) < 0.0 {
 			aReward += 1.0
+		} else {
+			bReward += 1.0
 		}
 	}
-	fmt.Printf("Random vs Random %f", aReward/2.0)
+	return aReward / 200.0, bReward/200.0
+}
+
+func TestRandomGame(t *testing.T) {
+	pA := RandomPlayer{}
+	pB := RandomPlayer{}
+	resA, resB := comparePlayers(pA, pB)
+	fmt.Printf("Random vs Random %f - %f", resA, resB)
 }
 
 func TestRandomvsproba(t *testing.T) {
 	pA := ProbabilistPlayer{}
 	pB := RandomPlayer{}
-	aReward := 0.0
 	NUM_SIMS  = 10_000
-	for i:=0; i<100; i++ {
-		if i%2==0 {
-			aReward += PlayGame(pA, pB)
-		} else {
-			aReward += -PlayGame(pB, pA)
-		}
-	}
-	fmt.Printf("Probabilist vs Random %f", aReward)
+	resA, resB := comparePlayers(pA, pB)
+	fmt.Printf("Random vs Random %f - %f", resA, resB)
 }
 
 func TestProba(t *testing.T) {
 	pA := ProbabilistPlayer{}
 	pB := ProbabilistPlayer{}
-	aReward := 0.0
 	NUM_SIMS  = 10_000
-	for i:=0; i<100; i++ {
-		if i%2==0 {
-			aReward += PlayGame(pA, pB)
-		} else {
-			aReward += -PlayGame(pB, pA)
-		}
-	}
-	fmt.Printf("Probabilist vs Probabilist %f", aReward)
+	resA, resB := comparePlayers(pA, pB)
+	fmt.Printf("Random vs Random %f - %f", resA, resB)
 }
